fa598ecc50ca80fd8740b91bf4eb9bd8b004f663
layyback <18511759309@163.com>
2022-01-19 17:17:02 +0800

    feat:添加ci文件

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,15 @@
 const path = require("path");
 module.exports = {
+  publicPath: "./",
+  productionSourceMap: false,
+  filenameHashing: true,
   pluginOptions: {
     "style-resources-loader": {
       preProcessor: "less",
       patterns: [path.resolve(__dirname, "./src/assets/style/global.less")]
     }
+  },
+  css: {
+    extract: false
   }
 };
